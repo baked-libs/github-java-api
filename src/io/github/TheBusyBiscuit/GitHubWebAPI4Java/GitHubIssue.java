1e0dea0d15a36b8bec02f98ea47472d44c327b13
TheBusyBiscuit <mrCookieSlime@gmail.com>
2017-03-18 11:48:22 +0100

    Added Issues class

---
+++ src/io/github/TheBusyBiscuit/GitHubWebAPI4Java/GitHubIssue.java
@@ -0,0 +1,75 @@
+package io.github.TheBusyBiscuit.GitHubWebAPI4Java;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import io.github.TheBusyBiscuit.GitHubWebAPI4Java.annotations.GitHubAccessPoint;
+
+public class GitHubIssue extends GitHubObject {
+	
+	private GitHubRepository repo;
+	
+	public GitHubIssue(GitHubWebAPI api, GitHubRepository repo, int number) {
+		super(api, repo, "/issues/" + number);
+		
+		this.repo = repo;
+	}
+	
+	public GitHubIssue(GitHubWebAPI api, GitHubRepository repo, int number, JsonElement response) {
+		super(api, repo, "/issues/" + number);
+
+		this.repo = repo;
+		this.minimal = response;
+	}
+
+	public GitHubIssue(GitHubObject obj) {
+		super(obj);
+	}
+	
+	@Override
+	public String getRawURL() {
+		return ".*repos/.*/.*/issues/.*";
+	}
+	
+	@GitHubAccessPoint(path = "@number", type = Integer.class)
+	public int getNumber() throws IllegalAccessException {
+		JsonElement element = getResponse(false);
+		
+		if (element == null) {
+			throw new IllegalAccessException("Could not connect to '" + getURL() + "'");
+		}
+		JsonObject response = element.getAsJsonObject();
+		
+		return isInvalid(response, "number") ? null: response.get("number").getAsInt();
+	}
+	
+	@GitHubAccessPoint(path = "@id", type = Integer.class)
+	public int getUID() throws IllegalAccessException {
+		JsonElement element = getResponse(false);
+		
+		if (element == null) {
+			throw new IllegalAccessException("Could not connect to '" + getURL() + "'");
+		}
+		JsonObject response = element.getAsJsonObject();
+		
+		return isInvalid(response, "id") ? null: response.get("id").getAsInt();
+	}
+
+	@GitHubAccessPoint(path = "@user", type = GitHubUser.class)
+	public GitHubUser getUser() throws IllegalAccessException {
+		JsonElement element = getResponse(false);
+		
+		if (element == null) {
+			throw new IllegalAccessException("Could not connect to '" + getURL() + "'");
+		}
+		JsonObject response = element.getAsJsonObject();
+		
+		return isInvalid(response, "user") ? null: new GitHubUser(api, response.get("user").getAsJsonObject().get("login").getAsString(), response.get("owner").getAsJsonObject());
+	}
+	
+	@GitHubAccessPoint(path = "@repository_url", type = GitHubRepository.class)
+	public GitHubRepository getRepository() {
+		return this.repo;
+	}
+
+}
